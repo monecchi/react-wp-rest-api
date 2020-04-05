4fc29d75a40592cbcd7d9576f016f4220ea5f897
monecchi <fornacciari@gmail.com>
2020-04-05 17:58:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wp-rest-api
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wp-rest-api)
---
+++ Restaurant.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+import useFetch from './src/useFetch';
+
+export default function RestaurantData() {
+    const restaurantData = useFetch('https://pizzariameurancho.com.br/wp-json/mrp/v1/stores/');
+    let storedata = []
+    let city
+    
+    storedata.push(restaurantData)
+
+  return (
+    <div className="container">
+      {restaurantData && restaurantData.map((store, index) => (
+
+        city = store.slug,
+
+      <div className={'row'} key={index}>
+              
+            { console.log(storedata) }
+
+            <div className={'col-12'}>
+                <div>
+                  <h4>Store Slug: <span className="textSecondary" dangerouslySetInnerHTML={{ __html:store.slug }}></span></h4>
+                  <h4>StoreID: <span dangerouslySetInnerHTML={{ __html: store.id }} /></h4>
+                  <ul>
+                    <li>Telefone: {store[city].formatted_phone}</li>
+                    <li>Loja aberta? {store[city].is_open}</li>
+                  </ul>
+                </div>
+            </div>
+      </div>
+      ))}
+    </div>
+  );
+}
