887a2545087743715febe5c24e0a712e6fa83266
monecchi <fornacciari@gmail.com>
2020-06-09 20:12:02 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
-import { ReactDOM, render } from 'react-dom';
+import React, { Component } from "react";
+import { ReactDOM, render } from "react-dom";
 // Bootstrap stuff
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
-import Hello from './Hello';
-import RestaurantData from './Restaurant';
+import Hello from "./Hello";
+import RestaurantData from "./Restaurant";
 
 // Importing the Bootstrap CSS
 // Import Custom CSS
-import './App.scss';
+import "./App.scss";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Meu Rancho'
+      name: "Meu Rancho"
     };
   }
 
   render() {
     return (
       <div>
-            <Navbar expand="lg" variant="light" bg="light">
-  <Container>
-    <Navbar.Brand href="#">Meu Rancho</Navbar.Brand>
-  </Container>
-</Navbar>
+        <Navbar expand="lg" variant="light" bg="light">
+          <Container>
+            <Navbar.Brand href="#">Meu Rancho</Navbar.Brand>
+          </Container>
+        </Navbar>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-         <RestaurantData />
+        <p>Start editing to see some magic happen :)</p>
+        <RestaurantData />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
