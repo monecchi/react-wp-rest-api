b681b38408b0886b994bbbc811f7ee14141b7611
monecchi <fornacciari@gmail.com>
2020-06-09 20:10:36 -0300

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { ReactDOM, render } from 'react-dom';
+// Bootstrap stuff
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+
 import Hello from './Hello';
 import RestaurantData from './Restaurant';
-import './style.css';
+
+// Importing the Bootstrap CSS
+// Import Custom CSS
+import './App.scss';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Meu Rancho'
     };
   }
 
   render() {
     return (
       <div>
+            <Navbar expand="lg" variant="light" bg="light">
+  <Container>
+    <Navbar.Brand href="#">Meu Rancho</Navbar.Brand>
+  </Container>
+</Navbar>
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
