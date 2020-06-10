3bf30e0e40b4f0a03b429b67afca7356afc91e04
monecchi <fornacciari@gmail.com>
2020-06-10 00:33:46 -0300

    2 files modified

--- Restaurant.js
+++ Restaurant.js
@@ -3,8 +3,18 @@ import React, { useEffect, useState } from "react";
 import useFetch from "./src/useFetch";
 
 // Bootstrap stuff
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+// Modifying BS components - https://github.com/react-bootstrap/react-bootstrap/blob/v1.0.1/src/Card.json
+import createWithBsPrefix from 'react-bootstrap/createWithBsPrefix';
+import divWithClassName from 'react-bootstrap/divWithClassName';
+
+const DivStyledAsH5 = divWithClassName('h5');
+
+Card.Title = createWithBsPrefix('card-title', {
+  Component: DivStyledAsH5,
+});
 
 export default function RestaurantData() {
   const restaurantInfo = useFetch(
@@ -16,11 +26,27 @@ export default function RestaurantData() {
   storedata.push(restaurantInfo);
 
   return (
-    <div className="container">
-      { restaurantInfo && restaurantInfo.map( (store, index) => ( city = store.slug, (
-        
-              <div className={"row"} key={index}>
+    <div className="card-deck">
+      {restaurantInfo &&
+        restaurantInfo.map(
+          (store, index) => (
+            (city = store.slug),
+            (
+              <div className="col p-0 m-0">
                 {console.log(storedata)}
+
+                <Card key={index}>
+                  <Card.Body>
+                    <Card.Title className="text-capitalize mb-4" dangerouslySetInnerHTML={{ __html: 'Loja ' + store.slug }} />
+                    <Card.Subtitle className="mb-2 text-muted" dangerouslySetInnerHTML={{ __html: 'StoreID: ' + store.id }} />
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
 
                 <div className={"col-12"}>
                   <div>
@@ -35,7 +61,7 @@ export default function RestaurantData() {
                     </h4>
                     <h4>
                       StoreID:{" "}
-                      <span dangerouslySetInnerHTML={{ __html: store.id }} />
+                      <span  />
                     </h4>
                     <ul>
                       <li>Telefone: {store[city].formatted_phone}</li>
@@ -46,8 +72,7 @@ export default function RestaurantData() {
               </div>
             )
           )
-        )
-      }
+        )}
     </div>
   );
 }
