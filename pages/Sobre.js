29ba8f319d868372127e9eefa51c1f79f0891da4
monecchi <fornacciari@gmail.com>
2020-06-10 15:58:07 -0300

    1 file added

---
+++ pages/Sobre.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { ReactDOM, render } from "react-dom";
+
+// Bootstrap stuff
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Nav from "react-bootstrap/Nav";
+import Col from "react-bootstrap/Col";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Navbar from "react-bootstrap/Navbar";
+
+class Sobre extends Component {
+ render() {
+ return (
+    <div className="App">
+        <Navbar bg="brand" expand="lg" variant="dark">
+          <Navbar.Brand href="/">Sobre</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      <p className="App-intro">
+            Exemplo de Página Sobre :)
+       </p>
+    </div>
+ );
+ }
+}
+export default Sobre;
