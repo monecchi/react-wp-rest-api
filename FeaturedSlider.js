edd3c0080aec5deda19e3d4fdb9b3dd3c4d84785
monecchi <fornacciari@gmail.com>
2020-06-10 12:24:51 -0300

    1 file modified

--- FeaturedSlider.js
+++ FeaturedSlider.js
@@ -5,7 +5,7 @@ import Flickity from "react-flickity-component";
 import "flickity/css/flickity.css";
 
 const flickityOptions = {
-  initialIndex: 1
+  initialIndex: 0
 };
 
 function HomeCarousel() {
@@ -18,10 +18,31 @@ function HomeCarousel() {
       reloadOnUpdate // default false
       static // default false
     >
-      <img src="./assets/images/ifood-bebidas-50-2.png" />
-      <img src="./assets/images/ifood-capas-novas-retirar.png" />
-      <img src="./assets/images/ifood-capas-novas-selecao-ifood.png" />
-      
+
+    <a href="#">
+      <figure class="highlights-carousel__figure">
+        <img src="https://static-images.ifood.com.br/image/upload//discoveries/ifood-capas-almoco10-app-v01-feb2020.png" alt="Almoço bom e barato" className="highlights-carousel__image" />
+      </figure>
+    </a>
+
+    <a href="#">
+      <figure class="highlights-carousel__figure">
+        <img src="https://cdn.jsdelivr.net/gh/monecchi/react-wp-rest-api@master/assets/images/ifood-bebidas-50-2.png" alt="Almoço bom e barato" className="highlights-carousel__image" />
+      </figure>
+    </a>
+
+    <a href="#">
+      <figure class="highlights-carousel__figure">
+        <img src="https://cdn.jsdelivr.net/gh/monecchi/react-wp-rest-api@master/assets/images/ifood-capas-novas-selecao-ifood.png" alt="Almoço bom e barato" className="highlights-carousel__image" />
+      </figure>
+    </a>
+
+    <a href="#">
+      <figure class="highlights-carousel__figure">
+        <img src="https://cdn.jsdelivr.net/gh/monecchi/react-wp-rest-api@master/assets/images/GROC_ifood.png" alt="Almoço bom e barato" className="highlights-carousel__image" />
+      </figure>
+    </a>
+
     </Flickity>
   );
 }
