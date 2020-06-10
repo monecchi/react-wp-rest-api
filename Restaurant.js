e28b7188e374d7941cb81ad015a455d84867cc4b
monecchi <fornacciari@gmail.com>
2020-06-10 01:09:23 -0300

    1 file modified

--- Restaurant.js
+++ Restaurant.js
@@ -8,13 +8,13 @@ import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 
 // Modifying BS components - https://github.com/react-bootstrap/react-bootstrap/blob/v1.0.1/src/Card.json
-import createWithBsPrefix from 'react-bootstrap/createWithBsPrefix';
-import divWithClassName from 'react-bootstrap/divWithClassName';
+import createWithBsPrefix from "react-bootstrap/createWithBsPrefix";
+import divWithClassName from "react-bootstrap/divWithClassName";
 
-const DivStyledAsH5 = divWithClassName('h5');
+const DivStyledAsH5 = divWithClassName("h5");
 
-Card.Title = createWithBsPrefix('card-title', {
-  Component: DivStyledAsH5,
+Card.Title = createWithBsPrefix("card-title", {
+  Component: DivStyledAsH5
 });
 
 export default function RestaurantData() {
@@ -23,7 +23,7 @@ export default function RestaurantData() {
   );
   let storedata = [];
   let city;
-  let isOpen = '';
+  let isOpen = "";
 
   storedata.push(restaurantInfo);
 
@@ -32,48 +32,39 @@ export default function RestaurantData() {
       {restaurantInfo &&
         restaurantInfo.map(
           (store, index) => (
-            (city = store.slug, isOpen = ( store[city].is_open ) == 1 ? "Aberto" : "Fechado"),
+            ((city = store.slug),
+            (isOpen = store[city].is_open == 1 ? "Aberto" : "Fechado")),
             (
               <div className="col p-0 m-0">
                 {console.log(storedata)}
 
                 <Card key={index}>
                   <Card.Body>
-                    <Card.Title className="text-capitalize mb-4" dangerouslySetInnerHTML={{ __html: 'Loja ' + store.slug }} />
-                    <Card.Subtitle className="mb-2 text-muted" dangerouslySetInnerHTML={{ __html: 'StoreID: ' + store.id }} />
-                    <Card.Text>
-                      { store[city].formatted_address }
-                    </Card.Text>
-                    <Badge pill variant="danger">
-                      { isOpen }
-                    </Badge>{' '}
-                    <Button href="tel:{store[city].phone_raw}" variant="primary">{store[city].formatted_phone}</Button>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
+                    <Card.Title
+                      className="text-capitalize mb-4"
+                      dangerouslySetInnerHTML={{ __html: "Loja " + store.slug }}
+                    />
+                    <Card.Subtitle
+                      className="mb-2 text-muted"
+                      dangerouslySetInnerHTML={{
+                        __html: "StoreID: " + store.id
+                      }}
+                    />
+                    <Card.Text>{store[city].formatted_address}</Card.Text>
+
+                    <div className="d-flex justify-content-start align-items-center">
+                      <Badge className={"mb-3"} pill variant="danger">
+                        {isOpen}
+                      </Badge>{" "}
+                    </div>
+
+                    <div className="mb-2">Ligar na loja</div>
+
+                    <Button href="tel:{store[city].phone_raw}" variant="primary">
+                      {store[city].formatted_phone}
+                    </Button>
                   </Card.Body>
                 </Card>
-
-                <div className={"col-12"}>
-                  <div>
-                    <h4>
-                      Store Slug:{" "}
-                      <span>
-                        <code
-                          className="textSecondary"
-                          dangerouslySetInnerHTML={{ __html: store.slug }}
-                        />
-                      </span>
-                    </h4>
-                    <h4>
-                      StoreID:{" "}
-                      <span  />
-                    </h4>
-                    <ul>
-                      <li>Telefone: {store[city].formatted_phone}</li>
-                      <li>Loja aberta? {store[city].is_open}</li>
-                    </ul>
-                  </div>
-                </div>
               </div>
             )
           )
