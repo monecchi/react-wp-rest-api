852d096fdbfc97fa1fb48703c1d205d75eacc6cc
monecchi <fornacciari@gmail.com>
2020-06-11 01:43:48 -0300

    1 file modified

--- pages/NotFound.js
+++ pages/NotFound.js
@@ -10,6 +10,7 @@ import Button from "react-bootstrap/Button";
 // Custom components import
 import NavbarNav from "../components/navbar/Navbar.js"; // custom component uses Bootstrap navbar
 import ManWithFork from "../src/components/UI/Illustrations/EmptyState/ManWithFork";
+import MeuSvg from "../src/components/UI/Illustrations/mr-not-found-fork.svg";
 
 class NotFound extends Component {
   constructor() {
@@ -26,12 +27,24 @@ class NotFound extends Component {
         <Container>
           <Row>
             <Col sm={12}>
-              <div className={"d-flex w-100 flex-column justify-content-center align-items-center py-4"}>
-                <ManWithFork />
+              <div className="d-flex w-100 flex-column justify-content-center align-items-center py-4">
+                <div className="">
+                  <h1
+                    className="position-relative"
+                    style={{
+                      display: this.state.isShowing ? "inherit" : "none"
+                    }}
+                  >
+                    Cadê?!
+                  </h1>
+                  <ManWithFork />
+                </div>
                 <p className="text-muted text-center my-3">
                   Não foi possível encontrar o conteúdo ou página
                 </p>
-                <Link to="/" className={'btn btn-danger'}>Voltar</Link>
+                <Link to="/" className="btn btn-danger">
+                  voltar
+                </Link>
               </div>
             </Col>
           </Row>
