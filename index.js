d09c51d20a7aaf31d4ac7ff2cdeaf743138888fa
monecchi <fornacciari@gmail.com>
2020-06-27 16:37:00 -0300

    2 files modified

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // App
 import App from "./App";
+//import * as serviceWorker from './serviceWorker';
 import Sobre from "./Sobre";
 import NotFound from "./pages/NotFound";
 //import registerServiceWorker from './registerServiceWorker';
