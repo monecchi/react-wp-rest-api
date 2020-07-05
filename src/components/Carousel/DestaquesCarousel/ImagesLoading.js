fffe09047f75b9cbc693e2c4f9787ac6e8f91225
monecchi <fornacciari@gmail.com>
2020-07-05 15:54:31 -0300

    6 binaries uploaded

---
+++ src/components/Carousel/DestaquesCarousel/ImagesLoading.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const WithImageLoading = () => {
+  const [componentState, setComponentState] = useState({
+    imageStatus: "loading"
+  });
+
+  useEffect(() => {
+    const handleImageLoaded = () => {
+      setComponentState({ imageStatus: "loaded" });
+    };
+
+    const handleImageErrored = () => {
+      setComponentState({ imageStatus: "failed to load" });
+    };
+  }, [setComponentState]);
+
+  return (
+    <div>
+      <img
+        src={this.props.imageUrl}
+        onLoad={this.handleImageLoaded.bind(this)}
+        onError={this.handleImageErrored.bind(this)}
+      />
+      {this.state.imageStatus}
+    </div>
+  );
+};
