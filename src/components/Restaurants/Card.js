0b9c6cedaae0182655c28e00b5997c0c3395a74d
monecchi <fornacciari@gmail.com>
2020-07-08 15:33:19 -0300

    1 file modified

--- src/components/Restaurants/Card.js
+++ src/components/Restaurants/Card.js
@@ -24,12 +24,41 @@ const RestaurantCard = ({ slug }) => {
 
   if (store) {
     singleStore = store[0];
-    const { city } = singleStore.slug;
+    let { city } = singleStore.slug;
+    let aberto = store[city].is_open;
     //console.log(singleStore);
   }
   if (isLoading) return <div>Carregando...</div>;
   if (isError) return <div>Erro ao carregar Restaurante</div>;
-  return <div>Olá {singleStore[slug].address_city}!</div>;
+  return (
+    <div>
+      <a
+        className={aberto == 1 ? "restaurant-simple-card" : "restaurant-simple-card restaurant-simple-card--closed"}
+        role="link"
+        href={"/"+singleStore[slug]}
+        key={singleStore[slug].id}
+      >
+        <div className="restaurant-logo-container">
+          <img
+            className="restaurant-logo"
+            src={singleStore[slug].resources.logo_url}
+            alt={"Meu Rancho " + singleStore[slug].address_city}
+          />
+        </div>
+        <div className="restaurant-info-container">
+          <div className="restaurant-name">
+            {"Meu Rancho " + singleStore[slug].address_city}
+          </div>
+          <div
+            className="restaurant-delivery-time"
+            aria-label="De 23 a 33 minutos"
+          >
+            23-33 min
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default RestaurantCard;
