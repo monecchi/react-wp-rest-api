b4b51f1a0bdc2196521641d8589397f505803fa0
monecchi <fornacciari@gmail.com>
2020-07-25 12:57:12 -0300

    1 file modified

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -6,7 +6,7 @@ import { Container, Div } from "atomize";
 const Layout = ({ children, ...props }) => {
   return (
     <>
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div style={{ margin: "0 auto", maxWidth: 650, padding: "0 1rem" }}>
       {children}
     </div>
     </>
