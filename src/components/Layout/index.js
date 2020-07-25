1ec68ff49e2b4a5816e7e8a09a2230c7fe9182c8
monecchi <fornacciari@gmail.com>
2020-07-25 12:56:39 -0300

    1 file modified, 1 added

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Atomize
+import { Container, Div } from "atomize";
+
+const Layout = ({ children, ...props }) => {
+  return (
+    <>
+    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+      {children}
+    </div>
+    </>
+  );
+};
+
+export default Layout;
