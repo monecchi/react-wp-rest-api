a25eb2f24b4d540af6ab3e889b8e7cddf6059839
monecchi <fornacciari@gmail.com>
2020-07-28 09:11:08 -0300

    1 file modified

--- src/components/Footer/Slim/index.js
+++ src/components/Footer/Slim/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 // Atomize
-import { Div, Container, Text, Icon } from "atomize";
+import { Div, Container, Anchor, Text, Icon } from "atomize";
+
+const mediaLinks = [
+  {
+    icon: "Instagram",
+    link: "https://instagram.com/pizzariameurancho"
+  },
+  {
+    icon: "Facebook",
+    link: "https://www.facebook.com/pizzariameurancho"
+  },
+  {
+    icon: "Twitter",
+    link: "https://twitter.com/mranchopizzaria"
+  },
+  {
+    icon: "Github",
+    link: "https://twitter.com/mranchopizzaria"
+  }
+];
 
 const FooterSlim = () => (
   <Div tag="footer" shadow="inset_top_sm">
@@ -13,7 +32,31 @@ const FooterSlim = () => (
       p="1.5rem"
       m={{ t: "3rem", b: "0" }}
     >
-      <Text align="center">Pizaria Meu Rancho</Text>
+    <Container>
+        <Div d="flex" align="center" justify="center" flexDir="column">
+          <Text
+            tag="p"
+            textWeight="400"
+            textAlign="center"
+            m={{ b: "1rem" }}
+          >
+            Pizzaria Meu Rancho - Todos os direitos reservados
+          </Text>
+          <Div d="flex" align="center" justify="center">
+            {mediaLinks.map((link, index) => (
+              <Anchor href={link.link} target="_blank" key={index}>
+                <Icon
+                  name={link.icon}
+                  size="20px"
+                  hoverColor="info800"
+                  m={{ r: index !== mediaLinks.length - 1 && "1rem" }}
+                  cursor="pointer"
+                />
+              </Anchor>
+            ))}
+          </Div>
+        </Div>
+      </Container>
     </Div>
   </Div>
 );
